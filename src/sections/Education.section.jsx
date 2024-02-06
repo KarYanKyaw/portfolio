cfd9e64d755bf9e99c1863fee8d7f7400742dbe6
KarYanKyaw <karyankyaw02@gmail.com>
2024-02-07 03:53:29 +0630

    bg fixed

--- src/sections/Education.section.jsx
+++ src/sections/Education.section.jsx
@@ -1,9 +1,12 @@
 import { EducationTree, SectionHeader, Wrapper } from "@/components";
 import React from "react";
-
+import road from "../assets/road.jpg";
 const EducationSection = () => {
   return (
-    <section id="education" className=" min-h-screen py-24 md:py-32 education">
+    <section
+      id="education"
+      className={` min-h-screen bg-[${road}] py-24 md:py-32 bg-contain md:bg-cover  education`}
+    >
       <Wrapper>
         <div className="flex flex-col gap-7">
           <SectionHeader className="!text-neutral-800">Education</SectionHeader>
